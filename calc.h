a5bb29a823ff3435679cd8d23b9f1a21151ebcd8
Spuko <83892026+SpukoBR@users.noreply.github.com>
2024-09-21 02:35:59 -0300

    calc.h

---
+++ calc.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calc.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: peda-cos <peda-cos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 02:26:08 by peda-cos          #+#    #+#             */
+/*   Updated: 2024/09/21 02:35:39 by peda-cos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CALC_H
+# define CALC_H
+
+# include <unistd.h>
+
+int		ft_atoi(const char *str);
+
+int		ft_isspace(int c);
+
+void	ft_putchar_fd(char c, int fd);
+void	ft_putnbr_fd(int n, int fd);
+void	ft_putstr_fd(const char *s, int fd);
+
+int		add(int a, int b);
+int		subtract(int a, int b);
+int		multiply(int a, int b);
+int		divide(int a, int b);
+int		calculate(int num1, char operator, int num2);
+
+#endif
